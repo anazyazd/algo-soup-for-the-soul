f45967757c6f609074c3fbeb681d0db7d3f15713
anazyazd <anazyazd@gmail.com>
2023-02-14 23:55:27 -0800

    level order bst algo

--- data-structures.js
+++ data-structures.js
@@ -58,3 +58,38 @@ var rotateRight = function(head, k) {
     return head;
 };
 
+// ----- Binary Tree Level Order Traversal ----- //
+
+// Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: [[3],[9,20],[15,7]]
+
+
+//  * Definition for a binary tree node.
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    if (!root) return [];
+    const result = [];
+    const queue = [root];
+    while (queue.length){
+        const subArr = [];
+        for (let i = 0; i < queue.length; i++){
+            let node = queue.pop();
+            subArr.push(node.val);
+            if (node.left) queue.unshift(node.left);
+            if (node.right) queue.unshift(node.right);
+        }
+        result.push(subArr);
+    }
+    return result;
+};
+
