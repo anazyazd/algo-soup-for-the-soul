af766e7e55fc3998dd979e4e82b63c044e8fa889
anazyazd <anazyazd@gmail.com>
2023-02-20 15:01:17 -0600

    add two numbers, linked list algo

--- data-structures.js
+++ data-structures.js
@@ -66,7 +66,7 @@ var rotateRight = function(head, k) {
 // Output: [[3],[9,20],[15,7]]
 
 
-//  * Definition for a binary tree node.
+//   Definition for a binary tree node.
 function TreeNode(val, left, right) {
     this.val = (val===undefined ? 0 : val)
     this.left = (left===undefined ? null : left)
@@ -122,3 +122,86 @@ var levelOrder = function(root) {
     }
     return result
 };
+
+// ----- Minimum Depth of Binary Tree ----- //
+
+// Given a binary tree, find its minimum depth.
+// The min depth is the number of nodes along the shortest path from the root node down to the nearest lead node.
+
+// input: root = [3,9,20,null,null,15,7] 
+// output: 2
+
+
+//  Definition for a binary tree node.
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+// recursive approach
+var minDepth = function(root) {
+    if (!root) return 0;
+    if (!root.left) return minDepth(root.right) + 1;
+    if (!root.right) return minDepth(root.left) + 1;
+    return Math.min(minDepth(root.left), minDepth(root.right));
+};
+
+// ------ Add Two Numbers ------ //
+
+// You are given two non-empty linked lists representing two non-negatibe integers. 
+// The digits are stored in reverse order, and each of their nodes contain a single digit. 
+// Add the two numbers and return the sum as a linked list.
+
+// Assume the two numbers do not contain any leading zero except the number 0 itself.
+
+
+//  * Definition for singly-linked list.
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+// input: l1 = [2,4,3], l2 = [5,6,4]
+// output: [7,0,8] because 342 + 465 = 807, everything is backwards
+var addTwoNumbers = function(l1, l2) {
+    let head = null;
+    let temp = null;
+    let acc = 0;
+    while (l1 || l2){
+        let sum = acc;
+        if (l1) {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+        if (l2) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+        let node = new ListNode(Math.floor(sum) % 10);
+        acc = Math.floor(sum / 10);
+        if (!temp){
+            temp = head = node;
+        }
+        else {
+            temp.next = node;
+            temp = temp.next;
+        }
+    }
+    if (acc > 0){
+        temp.next = new ListNode(acc);
+    }
+    return head;
+};
+
